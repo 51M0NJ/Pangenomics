bcba5743a415b04458f6e89bb7a1fd2dac441263
Simon Jeanneau <46607739+51M0NJ@users.noreply.github.com>
2025-09-19 15:25:52 +0100

    Update 4_bakta_rebuild.sh

--- 4_bakta_rebuild.sh
+++ 4_bakta_rebuild.sh
@@ -1,1 +1,5 @@
-
+mamba activate BaktaEnv
+nohup nice find . -maxdepth 1 -name '*.bakta.json' -print0   | parallel -0 --jobs 94 '
+      base={= s/\.bakta\.json$// =};
+      bakta_io --output "/home/def-jacquesp/jeanneau/9-Pangenomics/sample_annotation_total/${base}_complete" --prefix "$base" "{}"
+' > nohup_alljson2complete.out &
